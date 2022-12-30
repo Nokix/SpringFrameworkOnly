72b2d39a758f2361dd7a77021dd766de76d86e1f
Viktor Reichert <viktor.reichert@conteco.gmbh>
2022-12-30 16:01:45 +0100

    @Component & Componentscan in xml

--- src/main/java/de/reichert/Doctor.java
+++ src/main/java/de/reichert/Doctor.java
@@ -1,25 +1,12 @@
 package de.reichert;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Doctor implements Staff{
-    private String  qualification;
-    private Nurse nurse;
-
-    public Doctor(String qualification, Nurse nurse) {
-        this.qualification = qualification;
-        this.nurse = nurse;
-    }
-
-    public Nurse getNurse() {
-        return nurse;
-    }
-
-    public String getQualification() {
-        return qualification;
-    }
 
     @Override
     public void assist() {
         System.out.println("Doctor is assisting.");
-        nurse.assist();
     }
 }
