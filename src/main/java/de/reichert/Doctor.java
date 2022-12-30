4c82452c0b6aed7482a365de00686c0629c2b0f4
Viktor Reichert <viktor.reichert@conteco.gmbh>
2022-12-30 15:25:44 +0100

    Constructor-Args XML-Config

--- src/main/java/de/reichert/Doctor.java
+++ src/main/java/de/reichert/Doctor.java
@@ -4,20 +4,17 @@ public class Doctor implements Staff{
     private String  qualification;
     private Nurse nurse;
 
+    public Doctor(String qualification, Nurse nurse) {
+        this.qualification = qualification;
+        this.nurse = nurse;
+    }
+
     public Nurse getNurse() {
         return nurse;
     }
 
-    public void setNurse(Nurse nurse) {
-        this.nurse = nurse;
-    }
-
     public String getQualification() {
         return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
     }
 
     @Override
