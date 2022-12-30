57fc29b8527b136982575d48aa5f7dbed3efe886
Viktor Reichert <viktor.reichert@conteco.gmbh>
2022-12-30 16:05:55 +0100

    Annotationbased Configuration

---
+++ src/main/java/de/reichert/BeanConfig.java
@@ -0,0 +1,9 @@
+package de.reichert;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "de.reichert")
+public class BeanConfig {
+}
--- src/main/java/de/reichert/Main.java
+++ src/main/java/de/reichert/Main.java
@@ -1,12 +1,15 @@
 package de.reichert;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext
-                = new ClassPathXmlApplicationContext("spring.xml");
+                = new AnnotationConfigApplicationContext(BeanConfig.class);
+        //         = new ClassPathXmlApplicationContext("spring.xml");
 
         Doctor doctor = applicationContext.getBean(Doctor.class);
         doctor.assist();
