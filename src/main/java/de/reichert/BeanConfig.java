8c07785fb32583efc888a349e5f1811fa46ce148
Viktor Reichert <viktor.reichert@conteco.gmbh>
2022-12-31 13:01:59 +0100

    @Value

--- src/main/java/de/reichert/BeanConfig.java
+++ src/main/java/de/reichert/BeanConfig.java
@@ -3,11 +3,13 @@ package de.reichert;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.beans.BeanProperty;
 
 @Configuration
 @ComponentScan
+@PropertySource("spring.properties")
 public class BeanConfig {
 
 }
--- src/main/java/de/reichert/Doctor.java
+++ src/main/java/de/reichert/Doctor.java
@@ -1,6 +1,7 @@
 package de.reichert;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -8,19 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 //@Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class Doctor implements Staff{
+
+    @Value("${system}")
     private String name;
-
-    @Autowired
-    private Staff handyman;
-    // private Staff staff // Führt zu Fehler
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public void assist() {
         System.out.println("Doctor "+ name +" is assisting.");
-        handyman.assist();
     }
 }
